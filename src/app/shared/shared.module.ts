f1adf713c84c72bcecffa6afca114281254f49ae
lucia aguirre <guerrolucia@gmail.com>
2024-06-20 11:24:23 +0200

    feat: generates pipe example

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { ExamplePipe } from './pipes/example.pipe';
 
 @NgModule({
-  declarations: [TestComponentComponent],
+  declarations: [TestComponentComponent, ExamplePipe],
   imports: [CommonModule],
   exports: [TestComponentComponent],
 })
